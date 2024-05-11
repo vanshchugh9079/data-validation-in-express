59ddf809500d2fe2216aeb15ffb1c756fd27c6cd
vanshchugh9079 <vanshchugh67@gmail.com>
2024-05-11 07:56:22 +0530

    custom error messege

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ let app = express()
 dotenv.config()
 app.use(express.json())
 let { PORT } = process.env;
-app.post("/", body("name").isAlpha(), (req, res) => {
-    let result = validationResult(req)
-    if (result.isEmpty()) {
-        console.log(result);
-
-        res.status(200).json({ message: "success" })
-    }
-    else {
-        res.status(400).json({ message: result.errors[0].msg })
-    }
-
-})
+// app.post("/", body("name").isAlpha(), (req, res) => {
+//     let result = validationResult(req)
+//     if (result.isEmpty()) {
+//         console.log(result);
+//         res.status(200).json({ message: "success" })
+//     }
+//     else {
+//         res.status(400).json({ message: result.errors[0].msg })
+//     }
+// })
+//  custom messege validation
+app.post("/", body("password")
+    .isLength({ min: 6 })
+    .withMessage("password should me mininmum 6 chracter")
+    .matches(/\d/)
+    .withMessage("paswoord contain must one digit"),
+    (req, res) => {
+        let errors = validationResult(req)
+        if(errors.isEmpty()){
+            res.json({
+                message: "your acccount is successfully registered"
+            })
+        }
+        else{
+            res.status(400).json({
+                message: errors.errors[0].msg
+            })
+        }
+    })
 app.listen(PORT)
